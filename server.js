2291227cbadb0e744a47670661127fa4bc1c5355
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-23 12:53:53 +0100

    git ignore

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const PORT = 8000;
 
 
 const server = http.createServer((req, res) => {
-    res.end('Hey Baby world');
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
+    res.writeHead(500, {'Content-Type': 'application/json'});
+
+    // res.end('<h1> Haeyy sugar world </h1>');
+    res.end(JSON.stringify({message: 'Server Error'}));
 
 });
 
 server.listen(PORT, () => {
-    console.log(`Baby Server running on ${PORT}`);
+    console.log(`Bab123443221y Server running on ${PORT}`);
 });
