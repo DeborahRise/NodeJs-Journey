bf1d322c8cf5a4df8b9df50ad92f641357013920
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-25 16:56:22 +0100

    Added DemoOs.js for OS module examples

---
+++ DemoOs.js
@@ -0,0 +1,12 @@
+import os from 'os';
+
+// gives info about my pc. useful for creating systems or apps that 
+// interacts with the PC
+
+// userInfo()
+console.log(os.userInfo());
+
+// smae applies for totalmem()
+// freemem();
+// cpus()
+console.log(os.cpus());
