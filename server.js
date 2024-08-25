5ab3a7c54b7d7817745b31ff3dfffc592ea26d44
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-25 07:01:50 +0100

    Modify server.js to fix JSON stringify issue and improve error handling

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-import http from 'http';
-const PORT = 8000;
+// REST API THE BASICS
+import { createServer} from 'http';
+import { set } from 'immutable';
+import { stringify } from 'querystring';
 
+const PORT = process.env.PORT;
+const users = [
+    {id: 1, name: 'Tory'},
+    {id: 2, name: 'Dub'},
+    {id: 3, name: 'Deb'}
+]
 
-const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
-    res.writeHead(500, {'Content-Type': 'application/json'});
-
-    // res.end('<h1> Haeyy sugar world </h1>');
-    res.end(JSON.stringify({message: 'Server Error'}));
-
+// 
+const server = createServer((req, res) => {
+    if (req.url === '/api/users' && req.method === 'GET') {
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(users))
+        res.end();
+    } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+        // FETCHING a single user, using regex.
+        const id = req.url.split('/')[3];
+        const user = users.find((user) => user.id === parseInt(id));
+        res.setHeader('Content-Type', 'application/json');
+        if (user) {
+            res.write(JSON.stringify({user}))
+        } else {
+            res.write(JSON.stringify({message: 'user not found'}))
+        }
+        res.end();
+    } else {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 404
+        res.write( JSON.stringify({message: 'Not Found'}))
+        res.end();
+    }
 });
 
 server.listen(PORT, () => {
-    console.log(`Bab123443221y Server running on ${PORT}`);
-});
+    console.log('Rise Server');
+})
